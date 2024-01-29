a3dafc8e6aee03f4cf60e39b7585be8b78d6ede8
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2024-01-29 20:13:15 +0100

    big stuff

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function getElementByXPath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+const latestAside = document.getElementById("latest-aside");
+const latestHeader = getElementByXPath("//header[@id=\"latest-header\"]/strong");
+let latestVersion = latestAside.innerHTML.split('\n')[0].substring(2);
+latestHeader.innerHTML = latestVersion  ;
+console.log(latestHeader.innerHTML)
+
+// console.log(latest.innerHTML.replace(/<\/?[^>]+(>|$)/g, ""))
+// "This regex looks for <, an optional slash /, one or more characters that are not >, then either >
+// or $ (the end of the line)""
+// why yes, i yoinked that from scrapoverflow
